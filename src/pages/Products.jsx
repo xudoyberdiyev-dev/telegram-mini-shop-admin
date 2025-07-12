973f551e60029bea6ccaa5dbdbc8d99ad27078d0
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-07-12 17:13:02 +0500

    vv

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -61,7 +61,7 @@ export const Products = () => {
                 return;
             }
 
-            const maxSizeInMB = 5;
+            const maxSizeInMB = 10;
             const maxSizeInBytes = maxSizeInMB * 1024 * 1024;
 
             if (file.size > maxSizeInBytes) {
